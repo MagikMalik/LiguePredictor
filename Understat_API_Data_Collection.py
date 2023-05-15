1a2e341bedb1664c815aff81e8265dbdbf725f8b
MagikMalik <131989098+MagikMalik@users.noreply.github.com>
2023-05-15 17:46:59 +0200

    Update Understat_API_Data_Collection.py

--- Understat_API_Data_Collection.py
+++ Understat_API_Data_Collection.py
@@ -197,7 +197,7 @@ def get_weighted_goals(games_lookback, teams, team_data_dict, Use_xG):
             avg_wtd_goal_AwayTeam = wtd_goal / games_lookback
         print(team)
     if avg_wtd_goal_HomeTeam == 0.0 or avg_wtd_goal_AwayTeam == 0.0:
-        raise ValueError('The Weighted Average Goals Were Not Computed Correctly For the Teams in Question')
+        raise ValueError('The Weighted Average Goals Were Not Computed Correctly For the Teams in Question/' + teams[0]+'/'+teams[1])
 
     return avg_wtd_goal_HomeTeam, avg_wtd_goal_AwayTeam, wtd_goal_series
 
